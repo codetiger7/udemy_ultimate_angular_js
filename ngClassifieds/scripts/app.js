4244d1540fc3fc92168533adb99060497d7a13ba
codetiger7 <codetiger@icloud.com>
2016-08-07 16:14:05 +0200

    33 refactoring the classifies route

--- ngClassifieds/scripts/app.js
+++ ngClassifieds/scripts/app.js
@@ -7,22 +7,9 @@ angular
   .accentPalette('orange');
 
   $stateProvider
-  .state('one', {
-    url: '/stateone',
-    template: '<h1>{{ stateone.message }}</h1>',
-    controller: 'stateOneCtrl as stateone'
-  })
-  .state('two', {
-    url: '/statetwo',
-    template: '<h1>State Two</h1> <md-button ui-sref="two.more">Go to nested state</md-button><ui-view></ui-view>'
-  })
-  .state('two.more', {
-    url: '/more',
-    template: "<p>This is the deeper state</p>"
+  .state('classifieds', {
+    url: '/classifieds',
+    templateUrl: 'components/classifieds/classifieds.tpl.html',
+    controller: 'classifiedsCtrl as vm'
   });
-
-})
-.controller('stateOneCtrl', function() {
-  var vm = this;
-  vm.message="Hello, from state one";
 });
